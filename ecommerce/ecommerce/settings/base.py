9783aa238d24d6d222cac7c060caa0581a0cf0d2
mayejacob <mayejacob3@gmail.com>
2025-04-15 20:22:52 +0100

    updated the secret key

--- ecommerce/ecommerce/settings/base.py
+++ ecommerce/ecommerce/settings/base.py
@@ -5,12 +5,10 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!tvfkpi4yvyv6il6p%%dc1-21+e-o@=lf6%kpk=(gx$o25e&j='
+SECRET_KEY = '+r^-n*&v5au@s+g0#bhbdl8(5l_++nrig-=7_g%b@c=^$5+ewx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -52,18 +50,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
