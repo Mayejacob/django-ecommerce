1b06ee9ff9d7da5267a67b12536fbf47ece87b00
mayejacob <mayejacob3@gmail.com>
2025-07-02 12:06:57 +0100

    updated

--- ecommerce/ecommerce/settings/local.py
+++ ecommerce/ecommerce/settings/local.py
@@ -1,8 +1,12 @@
 from .base import *
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+ALLOWED_HOSTS = [
+    "ecommerce-bold-bird-4463.fly.dev",
+]
--- ecommerce/ecommerce/settings/production.py
+++ ecommerce/ecommerce/settings/production.py
@@ -1,5 +1,7 @@
 from .base import *
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "ecommerce-bold-bird-4463.fly.dev",
+]
 
 DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
